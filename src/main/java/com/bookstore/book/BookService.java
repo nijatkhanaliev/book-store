941dc68332d9a2f3ba12964b629b977305b3299e
nijat <khanalievn@gmail.com>
2025-09-06 01:01:44 +0400

    BookService created

---
+++ src/main/java/com/bookstore/book/BookService.java
@@ -0,0 +1,10 @@
+package com.bookstore.book;
+
+import com.bookstore.common.PageResponse;
+
+public interface BookService {
+    PageResponse<BookResponse> getAllSharableBook(int page, int size);
+    PageResponse<BookResponse> getAllArchivedBookByOwner(int page, int size);
+    void archiveBook(Long bookId);
+    void makeBookShareable(Long bookId);
+}
