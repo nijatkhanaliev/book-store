0789d83f8d6c433f862c68ce37b514930e452290
nijat <khanalievn@gmail.com>
2025-09-06 01:00:05 +0400

    BookController created

---
+++ src/main/java/com/bookstore/book/BookController.java
@@ -0,0 +1,14 @@
+package com.bookstore.book;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+@RequiredArgsConstructor
+public class BookController {
+
+//    public ResponseEntity<>
+
+}
