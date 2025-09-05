2eb15b6285746e630fd393c162b8814aac9e1947
nijat <khanalievn@gmail.com>
2025-09-06 01:00:54 +0400

    BookRepository created

---
+++ src/main/java/com/bookstore/book/BookRepository.java
@@ -0,0 +1,15 @@
+package com.bookstore.book;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @Query("""
+           select b from Book b where b.owner.id != :id
+        """)
+    Page<Book> findAll(Pageable pageable, @Param("id") Long currentUserId);
+}
