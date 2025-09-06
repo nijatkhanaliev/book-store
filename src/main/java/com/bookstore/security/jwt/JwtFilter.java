9207ee865fd2bb4d79b86ed0be1350d0c50cff65
nijat <khanalievn@gmail.com>
2025-09-07 00:13:42 +0400

    Jwt filter class created

---
+++ src/main/java/com/bookstore/security/jwt/JwtFilter.java
@@ -0,0 +1,32 @@
+package com.bookstore.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader!=null && authHeader.startsWith("Bearer ")){
+            String jwtToken = authHeader.substring(7);
+            String email;
+
+        }
+
+        /*
+            Authorization: Bearer dakdasdnsadmaskdnkasndasdnaskdnaskdnsadkn
+         */
+
+    }
+}
