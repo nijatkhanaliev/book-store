2c4ecefdb09173bc038c7e3274bc86839fe95e49
nijat <khanalievn@gmail.com>
2025-09-09 00:57:20 +0400

    getting all shareable book method created

--- src/main/java/com/bookstore/book/BookService.java
+++ src/main/java/com/bookstore/book/BookService.java
@@ -4,7 +4,10 @@ import com.bookstore.common.PageResponse;
 
 public interface BookService {
     PageResponse<BookResponse> getAllSharableBook(int page, int size);
+
     PageResponse<BookResponse> getAllArchivedBookByOwner(int page, int size);
+
     void archiveBook(Long bookId);
+
     void makeBookShareable(Long bookId);
 }
