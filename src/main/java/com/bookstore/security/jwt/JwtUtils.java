7280f1efd6085ebec65c8100f859b6fe31e76ede
nijat <khanalievn@gmail.com>
2025-09-08 21:49:51 +0400

    SecretKey created

--- src/main/java/com/bookstore/security/jwt/JwtUtils.java
+++ src/main/java/com/bookstore/security/jwt/JwtUtils.java
@@ -1,11 +1,37 @@
 package com.bookstore.security.jwt;
 
+import com.bookstore.user.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
 
 @Service
 public class JwtUtils {
 
+    @Value("${application.jwt.secret-key-str}")
+    private String secretKeyStr;
 
+    private SecretKey secretKey;
+
+    @PostConstruct
+    void init(){
+        byte[] decodedSecretKey = Decoders.BASE64.decode(secretKeyStr);
+        this.secretKey = Keys.hmacShaKeyFor(decodedSecretKey);
+    }
+
+
+    public String createJwt(User user){
+//        return Jwts.builder()
+//                .subject(user.getEmail())
+//                .claim("id",)
+
+        return null;
+    }
 
 
 }
