60ef88d089b5b957a8381f0b30981707232db15b
nijat <khanalievn@gmail.com>
2025-09-09 00:07:06 +0400

    RedisConfig created

---
+++ src/main/java/com/bookstore/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.bookstore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory());
+
+        return redisTemplate;
+    }
+
+}
