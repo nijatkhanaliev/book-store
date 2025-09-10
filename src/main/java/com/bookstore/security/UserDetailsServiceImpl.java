7dad17f1f9e1897a9d85d7c4a4e4ae22efcd7a92
nijat <nicatxanaliev@gmail.com>
2025-09-10 19:09:42 +0400

    UserDetailsServiceImpl created

---
+++ src/main/java/com/bookstore/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.bookstore.security;
+
+import com.bookstore.user.User;
+import com.bookstore.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+           User user = userRepository.findByEmail(email)
+                    .orElseThrow(()-> new UsernameNotFoundException("User not exists"));
+
+        return new UserDetailsImpl(user);
+    }
+
+}
