982939ffb54374c15b4974f3afbca34d3011465c
nijat <nicatxanaliev@gmail.com>
2025-09-11 00:29:05 +0400

    RefreshTokenRequest class created

--- src/main/java/com/bookstore/auth/RefreshTokenRequest.java
+++ src/main/java/com/bookstore/auth/RefreshTokenRequest.java
@@ -1,10 +1,12 @@
 package com.bookstore.auth;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class RefreshTokenRequest {
+    @NotNull
     private String refreshToken;
 }
