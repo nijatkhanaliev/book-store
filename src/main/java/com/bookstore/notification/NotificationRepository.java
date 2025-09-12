a353faf8c3ae9e2454d87a11a96aac1c4272c08c
nijat <nicatxanaliev@gmail.com>
2025-09-12 21:54:07 +0400

    NotificationRepository created

---
+++ src/main/java/com/bookstore/notification/NotificationRepository.java
@@ -0,0 +1,6 @@
+package com.bookstore.notification;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
