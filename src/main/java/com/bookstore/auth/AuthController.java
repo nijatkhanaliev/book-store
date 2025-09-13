14c9c066ab7f27d9c8c36ecb85057d491a2b308b
nijat <nicatxanaliev@gmail.com>
2025-09-14 01:56:44 +0400

    AuthController created

--- src/main/java/com/bookstore/auth/AuthController.java
+++ src/main/java/com/bookstore/auth/AuthController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,12 +18,12 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@Valid AuthRequest authRequest) {
+    public ResponseEntity<AuthResponse> login(@RequestBody  @Valid AuthRequest authRequest) {
         return ResponseEntity.ok(authService.login(authRequest));
     }
 
     @PostMapping("/register")
-    public ResponseEntity<Void> register(@Valid RegisterRequest registerRequest) {
+    public ResponseEntity<Void> register(@RequestBody @Valid RegisterRequest registerRequest) {
         authService.register(registerRequest);
         return ResponseEntity.status(CREATED).build();
     }
@@ -34,7 +35,7 @@ public class AuthController {
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<AuthResponse> refresh(@Valid RefreshTokenRequest request){
+    public ResponseEntity<AuthResponse> refresh(@RequestBody @Valid RefreshTokenRequest request){
         return ResponseEntity.ok(authService.refresh(request));
     }
 
