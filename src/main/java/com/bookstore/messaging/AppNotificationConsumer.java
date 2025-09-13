4cbb2523e50ff28f287743be4b1174164ff06230
nijat <nicatxanaliev@gmail.com>
2025-09-14 01:58:13 +0400

    AppNotification class created

--- src/main/java/com/bookstore/messaging/AppNotificationConsumer.java
+++ src/main/java/com/bookstore/messaging/AppNotificationConsumer.java
@@ -1,17 +1,22 @@
 package com.bookstore.messaging;
 
 import com.bookstore.notification.SellerApplicationCreatedEvent;
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class AppNotificationConsumer {
+
+    private final SimpMessagingTemplate messagingTemplate;
 
     @RabbitListener(queues = "notification-queue")
     private void consume(SellerApplicationCreatedEvent event){
-        Long userId = event.getUserId();
-        String userFullName = event.getUserFullName();
-
-
-
+//        Long userId = event.getUserId();
+//        String userFullName = event.getUserFullName();
+        messagingTemplate.convertAndSend("topic/admin-notifications", "new Notification for Admin");
     }
 
 }
